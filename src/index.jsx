17ffd27d3d8cdc41f36532011d60954a44f5f6e4
Leo Wong <leoelicos@gmail.com>
2023-05-08 09:40:51 +1000

    Replace init function without it

--- src/index.jsx
+++ src/index.jsx
@@ -8,30 +8,26 @@ import Suggestions from './components/Suggestions/index.jsx'
 import Results from './components/Results/index.jsx'
 import ErrorPage from './components/ErrorPage/index.jsx'
 
-function init() {
-  const domNode = document.getElementById('root')
-  const root = createRoot(domNode)
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Moovee />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: '/',
-          element: <Suggestions />,
-          errorElement: <ErrorPage />
-        },
-        {
-          path: '/results',
-          element: <Results />,
-          errorElement: <ErrorPage />
-        }
-      ]
-    }
-  ])
-  const routerProvider = <RouterProvider router={router} />
-  root.render(routerProvider)
-}
-
-init()
+const domNode = document.getElementById('root')
+const root = createRoot(domNode)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Moovee />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Suggestions />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/results',
+        element: <Results />,
+        errorElement: <ErrorPage />
+      }
+    ]
+  }
+])
+const routerProvider = <RouterProvider router={router} />
+root.render(routerProvider)
