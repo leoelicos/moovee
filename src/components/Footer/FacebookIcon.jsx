d96b197b2e49d2ee0b7688bdbc35feb1e860c310
Leo Wong <leoelicos@gmail.com>
2023-05-07 19:44:43 +1000

    Use facebook-icon

---
+++ src/components/Footer/FacebookIcon.jsx
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+
+export default function FacebookIcon() {
+  return (
+    <FontAwesomeIcon
+      className='facebook-icon'
+      icon={faFacebookF}
+    />
+  )
+}
