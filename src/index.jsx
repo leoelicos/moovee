387c3b031de1cecdbaa7b59df5f8d4c2c461084a
Leo Wong <leoelicos@gmail.com>
2023-05-10 22:20:23 +1000

    Simplify routing logic

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-/* routes */
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Moovee from './components/App.jsx'
 import Suggestions from './components/Suggestions/index.jsx'
 import Results from './components/Results/index.jsx'
 import ErrorPage from './components/ErrorPage/index.jsx'
 
-const domNode = document.getElementById('root')
-const root = createRoot(domNode)
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Moovee />,
-    errorElement: <ErrorPage />,
-    children: [
+createRoot(document.getElementById('root')).render(
+  <RouterProvider
+    router={createBrowserRouter([
       {
         path: '/',
-        element: <Suggestions />,
-        errorElement: <ErrorPage />
-      },
-      {
-        path: '/results',
-        element: <Results />,
-        errorElement: <ErrorPage />
+        element: <Moovee />,
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            path: '/',
+            element: <Suggestions />,
+            errorElement: <ErrorPage />
+          },
+          {
+            path: '/results',
+            element: <Results />,
+            errorElement: <ErrorPage />
+          }
+        ]
       }
-    ]
-  }
-])
-const routerProvider = <RouterProvider router={router} />
-root.render(routerProvider)
+    ])}
+  />
+)
