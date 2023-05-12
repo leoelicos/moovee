09232ff54a145afedbdad5594d952a41b3f3085f
Leo Wong <leoelicos@gmail.com>
2023-05-12 17:49:02 +1000

    Simplify functions

--- src/api/omdbapi__by-search.js
+++ src/api/omdbapi__by-search.js
@@ -1,18 +1,12 @@
 import axios from 'axios'
 
-const { REACT_APP_OMDB_KEY } = process.env
+const { REACT_APP_OMDB_KEY: key } = process.env
 
 export default async function OMDbAPIBySearch(string) {
   try {
-    if (string === undefined) {
-      throw new Error('omdbapiBySearch: No query')
-    }
-
-    if (REACT_APP_OMDB_KEY === undefined) {
-      throw new Error('omdbapiBySearch: No key')
-    }
-
-    const res = await query(id)
+    if (string === undefined) throw new Error('omdbapiBySearch: No query')
+    if (key === undefined) throw new Error('omdbapiBySearch: No key')
+    const res = await query(string)
     const data = extractData(res)
     return data
   } catch (error) {
@@ -21,12 +15,8 @@ export default async function OMDbAPIBySearch(string) {
   }
 }
 
-const query = async (id) =>
-  await axios('https://www.omdbapi.com', {
-    apikey: REACT_APP_OMDB_KEY,
-    type: 'movie',
-    s: str,
-    page: 1
-  })
+const uri = 'https://www.omdbapi.com'
+const options = { apikey: key, type: 'movie', page: 1 }
+const query = async (str) => await axios(uri, { ...options, s: str })
 
 const extractData = (res) => res.data.Search
