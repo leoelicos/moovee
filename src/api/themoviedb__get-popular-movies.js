1e4386c6e6d0a666b11ec8998e862e86fb9ce2da
Leo Wong <leoelicos@gmail.com>
2023-05-12 17:49:08 +1000

    Simplify functions

--- src/api/themoviedb__get-popular-movies.js
+++ src/api/themoviedb__get-popular-movies.js
@@ -1,14 +1,11 @@
 import axios from 'axios'
 
-const { REACT_APP_TMDB_KEY } = process.env
+const { REACT_APP_TMDB_KEY: key } = process.env
 
 export default async function TMDbAPIBySearch() {
   try {
-    if (!REACT_APP_TMDB_KEY) {
-      throw new Error('themoviedbGetPopularMovies: No Key')
-    }
+    if (!key) throw new Error('themoviedbGetPopularMovies: No Key')
     const res = await query()
-
     return extractTitles(res)
   } catch (error) {
     console.error(error)
@@ -16,13 +13,8 @@ export default async function TMDbAPIBySearch() {
   }
 }
 
-const query = async () => {
-  console.log({ REACT_APP_TMDB_KEY })
-
-  const result = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${REACT_APP_TMDB_KEY}`, { page: 1, language: 'en-US' })
-
-  console.log({ result })
-  return result
-}
+const uri = `https://api.themoviedb.org/3/movie/popular?api_key=${key}`
+const options = { page: 1, language: 'en-US' }
+const query = async () => await axios(uri, options)
 
 const extractTitles = (res) => res.data.results.map(({ title }) => title)
