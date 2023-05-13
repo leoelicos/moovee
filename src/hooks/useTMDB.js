daf05dd7b357f2374b6af4d8204958716d5c4608
Leo Wong <leoelicos@gmail.com>
2023-05-13 23:20:37 +1000

    Remove spacing

--- src/hooks/useTMDB.js
+++ src/hooks/useTMDB.js
@@ -3,19 +3,15 @@ import api from '../api/themoviedb__get-popular-movies.js'
 
 export default function useTMDB() {
   const [loading, setLoading] = useState(false)
-
   const [movieTitles, setMovieTitles] = useState([])
 
   const search = useCallback(async () => {
     setLoading(true)
-
     try {
       const retrievedMovieTitles = await api()
-      console.log({ retrievedMovieTitles })
       setMovieTitles(retrievedMovieTitles)
     } catch (error) {
       console.error(error)
-
       setMovieTitles([])
     } finally {
       setLoading(false)
