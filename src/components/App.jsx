425e0b24bc791c6447c519676c2dd9fe81e3e7b8
Leo Wong <leoelicos@gmail.com>
2023-05-15 11:04:41 +1000

    Provider only

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,11 @@
-import { Outlet } from 'react-router-dom'
-
-import Header from './Header/index.jsx'
-import Footer from './Footer/index.jsx'
-import Theme from './Theme.jsx'
-
-import '../style/index.css'
-import useOMDB from '../hooks/useOMDB.js'
-import { useSearchParams } from 'react-router-dom'
-import { useContext, useEffect } from 'react'
 import OMDBProvider from '../context/OMDBContext.jsx'
+import Moovee from './Moovee.jsx'
+import '../style/index.css'
 
-import { OMDBDispatchContext } from '../context/OMDBContext.jsx'
-
-export default function Moovee() {
-  const [searchParams] = useSearchParams()
-  const query = searchParams.get('q') || ''
-  const { omdbMovies, searchOMDB } = useOMDB()
-  const dispatchOMDB = useContext(OMDBDispatchContext)
-
-  useEffect(() => {
-    console.log('App useEffect', { query })
-
-    searchOMDB(query)
-    /* dispatch omdb search */
-    dispatchOMDB({
-      type: 'setLoading',
-      action: true
-    })
-
-    dispatchOMDB({
-      type: 'setMovies',
-      action: omdbMovies
-    })
-
-    return () => {
-      dispatchOMDB({
-        type: 'setLoading',
-        action: false
-      })
-    }
-  }, [query, dispatchOMDB, omdbMovies, searchOMDB])
-
+export default function ContextProvider() {
   return (
     <OMDBProvider>
-      <div className='moovee'>
-        <Theme>
-          <Header query={query} />
-          <main>
-            <Outlet />
-          </main>
-          <Footer />
-        </Theme>
-      </div>
+      <Moovee />
     </OMDBProvider>
   )
 }
