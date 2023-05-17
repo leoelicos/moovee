36e91754870f571d2555d130e5f8af784cf305d8
Leo Wong <leoelicos@gmail.com>
2023-05-17 23:23:23 +1000

    useEffect to get Query

--- src/components/Suggestions/index.jsx
+++ src/components/Suggestions/index.jsx
@@ -1,57 +1,47 @@
 import { useEffect } from 'react'
 import useTMDB from '../../hooks/useTMDB.js'
-import { Tag } from 'antd'
-import { useNavigate } from 'react-router-dom'
-import cleanQuery from '../Header/Search/cleanQuery.js'
 
-export default function Suggestions({ nav }) {
+import { useNavigate } from 'react-router-dom'
+import cleanQuery from '../../utils/cleanQuery.js'
+import PopularMovie from './PopularMovie.jsx'
+import SuggestionsHeading from './SuggestionsHeading.jsx'
+export default function Suggestions() {
   const { loading, movieTitles, search } = useTMDB()
   const colors = ['magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple']
+
   useEffect(() => {
+    console.log('useEffect Suggestions')
     search()
   }, [search])
+
   const navigate = useNavigate()
-  function goToResults(query) {
-    const cleanedQuery = cleanQuery(query)
+
+  const handleClick = (text) => {
+    const cleanedQuery = cleanQuery(text)
     const nextPage = `/results?q=${cleanedQuery}`
     navigate(nextPage)
   }
 
-  const handleClick = (text) => {
-    goToResults(text)
-  }
-
   if (loading) return 'loading'
+
   return (
-    <ul className='suggestions'>
-      {movieTitles.map((title, i) => {
-        const colorLastIdx = colors.length - 1
-        const colorIdx = i % colorLastIdx || colorLastIdx
-        const color = colors[colorIdx]
-        return (
-          <PopularMovie
-            title={title}
-            key={title}
-            color={color}
-            handleClick={handleClick}
-          />
-        )
-      })}
-    </ul>
-  )
-}
-function PopularMovie({ title, color, handleClick }) {
-  return (
-    <Tag
-      className='tmdb-tag'
-      bordered={false}
-      key={title}
-      color={color}
-      data-text={title}
-      onClick={(e) => {
-        handleClick(title)
-      }}>
-      {title}
-    </Tag>
+    <div className='suggestions'>
+      <SuggestionsHeading />
+      <ul className='list'>
+        {movieTitles.map((title, i) => {
+          const colorLastIdx = colors.length - 1
+          const colorIdx = i % colorLastIdx || colorLastIdx
+          const color = colors[colorIdx]
+          return (
+            <PopularMovie
+              title={title}
+              key={title}
+              color={color}
+              handleClick={handleClick}
+            />
+          )
+        })}
+      </ul>
+    </div>
   )
 }
