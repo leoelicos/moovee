9530b0f6a381e864dc058574b45bb4091a41dfe0
Leo Wong <leoelicos@gmail.com>
2023-05-17 23:20:59 +1000

    Return Moovee

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,6 @@
-import OMDBProvider from '../context/OMDBContext.jsx'
 import Moovee from './Moovee.jsx'
 import '../style/index.css'
 
-export default function ContextProvider() {
-  return (
-    <OMDBProvider>
-      <Moovee />
-    </OMDBProvider>
-  )
+export default function App() {
+  return <Moovee />
 }
