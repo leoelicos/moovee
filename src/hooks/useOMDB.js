0223695975e235b7fd7c0aed0347e382e1b25a69
Leo Wong <leoelicos@gmail.com>
2023-05-20 23:39:52 +1000

    Break down function parse() into smaller

--- src/hooks/useOMDB.js
+++ src/hooks/useOMDB.js
@@ -1,21 +1,7 @@
-//* useOMDB: loading and data states for OMDB API
-//? searchOMDB(str) OMDB By Search
-//? searchOMDBByID(id) OMDB By Id
-
 import { useCallback, useState } from 'react'
-const { REACT_APP_OMDB_KEY: key } = process.env
+import axios from 'axios'
 
-const parse = (imdbID, data) => ({
-  imdbID: imdbID,
-  poster: data.Poster === 'N/A' ? null : data.Poster,
-  title: data.Title === 'N/A' ? null : data.Title,
-  esrb: data.Rated === 'N/A' ? null : data.Rated,
-  year: data.Year === 'N/A' ? null : data.Year,
-  genre: data.Genre === 'N/A' ? null : data.Genre.split(', '),
-  actors: data.Actors || [],
-  plot: data.Plot === 'N/A' ? null : data.Plot,
-  imdbRating: data.Ratings?.length === 0 ? null : parseFloat(data.Ratings.find((r) => r.Source === 'Internet Movie Database').Value)
-})
+const { REACT_APP_OMDB_KEY: key } = process.env
 
 export default function useOMDB() {
   const [omdbLoading, setOmdbLoading] = useState(null)
@@ -25,11 +11,16 @@ export default function useOMDB() {
     console.log('searchOMDB', { str })
     setOmdbLoading(true)
     try {
+      if (str === undefined) throw new Error('searchOMDB: No query')
+      if (key === undefined) throw new Error('searchOMDB: No key')
+
       const res = await fetch(`https://www.omdbapi.com?apikey=${key}&type=movie&page=1&s=${str}`)
+
       const data = await res.json()
-      const { Search } = data
-      const movies = Search.filter((v) => v.hasOwnProperty('imdbID'))
-      setOmdbMovies(movies)
+      const movies = data.Search.filter((v) => v.hasOwnProperty('imdbID'))
+      const parsedMovies = movies.map(parseBySearch)
+      console.log({ parsedMovies })
+      setOmdbMovies(parsedMovies)
     } catch (error) {
       console.error(error)
     } finally {
@@ -37,5 +28,42 @@ export default function useOMDB() {
     }
   }, [])
 
-  return { omdbLoading, omdbMovies, searchOMDB }
+  const searchOMDBById = useCallback(async (id) => {
+    console.log('searchOMDBById', { id })
+    setOmdbLoading(true)
+    try {
+      if (id === undefined) throw new Error('searchOMDBById: No id')
+      if (key === undefined) throw new Error('searchOMDBById: No key')
+
+      const uri = 'https://www.omdbapi.com'
+      const params = { apikey: key, type: 'movie' }
+      const query = async (id) => await axios(uri, { params, i: id })
+      const res = await query(id)
+      const data = await res.data
+      const parsedMovie = parseById(data)
+      console.log({ parsedMovie })
+      setOmdbMovies((prev) => (prev.imdbID === id ? { ...prev, ...parsedMovie } : prev))
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setOmdbLoading(false)
+    }
+  }, [])
+
+  return { omdbLoading, omdbMovies, searchOMDB, searchOMDBById }
 }
+
+const parseBySearch = (data) => ({
+  imdbID: data.imdbID,
+  poster: data.Poster === 'N/A' ? null : data.Poster,
+  title: data.Title === 'N/A' ? null : data.Title,
+  year: data.Year === 'N/A' ? null : data.Year
+})
+
+const parseById = (data) => ({
+  esrb: data.Rated === 'N/A' ? null : data.Rated,
+  genre: data.Genre === 'N/A' ? null : data.Genre.split(', '),
+  actors: data.Actors || [],
+  plot: data.Plot === 'N/A' ? null : data.Plot,
+  imdbRating: data.Ratings?.length === 0 ? null : parseFloat(data.Ratings.find((r) => r.Source === 'Internet Movie Database').Value)
+})
