1d9b30311f6214956f55d54de0f6689cb23e6eca
Leo Wong <leoelicos@gmail.com>
2023-05-20 23:39:58 +1000

    Incorporate API logic

--- src/hooks/useTMDB.js
+++ src/hooks/useTMDB.js
@@ -1,5 +1,8 @@
 import { useCallback, useState } from 'react'
-import api from '../api/themoviedb__get-popular-movies.js'
+
+import axios from 'axios'
+import mockTMDB from '../test/mockTMDB.js'
+const { REACT_APP_TMDB_KEY: key } = process.env
 
 export default function useTMDB() {
   const [loading, setLoading] = useState(false)
@@ -8,7 +11,12 @@ export default function useTMDB() {
   const search = useCallback(async () => {
     setLoading(true)
     try {
-      const retrievedMovieTitles = await api()
+      const testing = true
+
+      let retrievedMovieTitles
+      if (testing) retrievedMovieTitles = mockTMDB
+      else retrievedMovieTitles = await TMDbAPIBySearch()
+
       setMovieTitles(retrievedMovieTitles)
     } catch (error) {
       console.error(error)
@@ -24,3 +32,20 @@ export default function useTMDB() {
     search
   }
 }
+
+async function TMDbAPIBySearch() {
+  try {
+    if (!key) throw new Error('themoviedbGetPopularMovies: No Key')
+    const res = await query()
+    return extractTitles(res)
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+const uri = `https://api.themoviedb.org/3/movie/popular?api_key=${key}`
+const options = { page: 1, language: 'en-US' }
+const query = async () => await axios(uri, options)
+
+const extractTitles = (res) => res.data.results.map(({ title }) => title)
