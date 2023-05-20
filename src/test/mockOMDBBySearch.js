64231c3f4660128ee60e7d282c51b2bb3fe96ad5
Leo Wong <leoelicos@gmail.com>
2023-05-21 00:01:38 +1000

    Update mock data

---
+++ src/test/mockOMDBBySearch.js
@@ -0,0 +1,39 @@
+const array = [
+  {
+    imdbID: 'tt6718170',
+    poster: 'https://m.media-amazon.com/images/M/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_SX300.jpg',
+    title: 'The Super Mario Bros. Movie',
+    year: '2023'
+  },
+  {
+    imdbID: 'tt8248964',
+    poster: null,
+    title: 'Mario & Luigi Reenact Last Name Reveal Scene from the Super Mario Bros. Movie',
+    year: '2017'
+  },
+  {
+    imdbID: 'tt12049816',
+    poster: 'https://m.media-amazon.com/images/M/MV5BMmQ4NGY4NTYtZTlhMC00MDdjLWEzMTgtZjFiM2EwM2EwMGYyXkEyXkFqcGdeQXVyMTA1NzU3MTk3._V1_SX300.jpg',
+    title: 'Super Mario Bros: The Movie',
+    year: '2018'
+  },
+  {
+    imdbID: 'tt12049852',
+    poster: 'https://m.media-amazon.com/images/M/MV5BMzFhMzAyZTQtNDg2Mi00YmYxLWFhYWYtZmUzYTM1ZjIxZGUwXkEyXkFqcGdeQXVyMTA1NzU3MTk3._V1_SX300.jpg',
+    title: 'Super Mario Bros the Movie 2',
+    year: '2018'
+  },
+  {
+    imdbID: 'tt12089480',
+    poster: 'https://m.media-amazon.com/images/M/MV5BNWQ2MjNlYTktNzY4OC00MWUyLThlZjgtZjRhZWEwM2FmZjVjXkEyXkFqcGdeQXVyMTA1NzU3MTk3._V1_SX300.jpg',
+    title: 'Super Mario Bros: The Movie - 01',
+    year: '2010'
+  },
+  {
+    imdbID: 'tt12097124',
+    poster: 'https://m.media-amazon.com/images/M/MV5BZWMwMWM1ZTUtYTNmYy00YzU3LTkxZTMtYTBmOTQ2NWIxZTEyXkEyXkFqcGdeQXVyMTA1NzU3MTk3._V1_SX300.jpg',
+    title: 'Super Mario Bros: The Movie 2 - 01',
+    year: '2013'
+  }
+]
+export default array
