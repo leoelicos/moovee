147257617d8272dd76ad23c7d025632d7d600ee4
Leo Wong <leoelicos@gmail.com>
2023-05-22 02:42:40 +1000

    Move main-related CSS here

--- src/components/Moovee.jsx
+++ src/components/Moovee.jsx
@@ -1,15 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import { useSearchParams, Outlet } from 'react-router-dom'
 
 import Header from './Header/index.jsx'
 import Footer from './Footer/index.jsx'
 import Theme from './Theme.jsx'
-
+import Modal from './Modal/index.jsx'
 import '../style/index.css'
+import Trailer from './Modal/Trailer.jsx'
+
+import { MovieContext } from '../context/index.jsx'
 
 export default function Moovee() {
   const [searchParams] = useSearchParams()
   const [query, setQuery] = useState(searchParams.get('q') || '')
+
+  const { youTubeData } = useContext(MovieContext)
+
   useEffect(() => {
     setQuery(searchParams.get('q') || '')
   }, [searchParams])
@@ -22,6 +28,9 @@ export default function Moovee() {
           <Outlet />
         </main>
         <Footer />
+        <Modal>
+          <Trailer uri={youTubeData} />
+        </Modal>
       </Theme>
     </div>
   )
