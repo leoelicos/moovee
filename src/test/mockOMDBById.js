5f6e646d9ad5c1dac59f0d7ad55aec3e99d36f1f
Leo Wong <leoelicos@gmail.com>
2023-05-22 02:43:37 +1000

    Mock data for specific search

---
+++ src/test/mockOMDBById.js
@@ -0,0 +1,41 @@
+const object = {
+  Title: 'The Super Mario Bros. Movie',
+  Year: '2023',
+  Rated: 'PG',
+  Released: '05 Apr 2023',
+  Runtime: '92 min',
+  Genre: 'Animation, Adventure, Comedy',
+  Director: 'Aaron Horvath, Michael Jelenic, Pierre Leduc',
+  Writer: 'Matthew Fogel',
+  Actors: 'Chris Pratt, Anya Taylor-Joy, Charlie Day',
+  Plot: 'The story of The Super Mario Bros. on their journey through the Mushroom Kingdom.',
+  Language: 'English',
+  Country: 'United States, Japan',
+  Awards: 'N/A',
+  Poster: 'https://m.media-amazon.com/images/M/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_SX300.jpg',
+  Ratings: [
+    {
+      Source: 'Internet Movie Database',
+      Value: '7.3/10'
+    },
+    {
+      Source: 'Rotten Tomatoes',
+      Value: '59%'
+    },
+    {
+      Source: 'Metacritic',
+      Value: '46/100'
+    }
+  ],
+  Metascore: '46',
+  imdbRating: '7.3',
+  imdbVotes: '92,622',
+  imdbID: 'tt6718170',
+  Type: 'movie',
+  DVD: 'N/A',
+  BoxOffice: '$518,079,325',
+  Production: 'N/A',
+  Website: 'N/A',
+  Response: 'True'
+}
+export default object
