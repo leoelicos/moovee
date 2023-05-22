0714412a938a35885a1e6c9de6b068f5cf0928d5
Leo Wong <leoelicos@gmail.com>
2023-05-22 23:55:20 +1000

    Remove hooks from context

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,11 +1,9 @@
 import { createContext, useEffect, useReducer } from 'react'
-import useYouTube from '../hooks/useYouTube'
 
 export const MovieContext = createContext(null)
 export const MovieDispatchContext = createContext(null)
 
 export default function MovieProvider({ children }) {
-  const { youTubeData, youTubeLoading, youTubeError, searchYouTube } = useYouTube()
   const initialState = {
     youTubeData: '',
     youTubeLoading: false,
@@ -34,6 +32,7 @@ export default function MovieProvider({ children }) {
 
 /* return updated state to useReducer */
 async function MovieReducer(state, { type, action }) {
+  console.log('MovieReducer', { type, action })
   switch (type) {
     case 'loadingTrue': {
       return { ...state, youTubeLoading: true }
@@ -53,12 +52,7 @@ async function MovieReducer(state, { type, action }) {
 
     case 'foundTrailer': {
       const { youTubeData } = action
-      if (!youTubeData) {
-        console.log('Missing youTubeData')
-        return
-      }
-      console.log('reducer', { youTubeData })
-      return { ...state, youTubeData }
+      return youTubeData ? { ...state, youTubeData } : state
     }
 
     case 'modalOpen': {
@@ -74,12 +68,3 @@ async function MovieReducer(state, { type, action }) {
     }
   }
 }
-
-function mockQuery() {
-  const dummyData = { data: { items: [{ id: { videoId: 'bKn-NdqSkU4' } }] } }
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(dummyData), 1000)
-  })
-}
-
-const parse = (res) => res?.data?.items?.[0]?.id?.videoId || null
