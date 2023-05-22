7623325f8248289f3710ebb9f0fa154a7f9f4653
Leo Wong <leoelicos@gmail.com>
2023-05-22 23:55:04 +1000

    Add active state to conditionally render poster and details

--- src/components/Results/Result/index.jsx
+++ src/components/Results/Result/index.jsx
@@ -1,13 +1,15 @@
-import { useContext } from 'react'
-import { Tag, Button } from 'antd'
+import { useContext, useState } from 'react'
+import { Tag, Button, Empty } from 'antd'
 import useYouTube from '../../../hooks/useYouTube'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFilm, faStar } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faFilm } from '@fortawesome/free-solid-svg-icons'
 import NoPosterLogo from '../../../images/noposter.png'
 import { MovieContext, MovieDispatchContext } from '../../../context'
+import Stars from './Stars'
 
-export default function Result({ poster, title, esrb, year, genre, actors, plot, imdbRating, idx }) {
+export default function Result({ poster, title, esrb, year, genre, actors, plot, imdbRating }) {
+  const [active, setActive] = useState(false)
   const dispatch = useContext(MovieDispatchContext)
   const { searchYouTube } = useYouTube(dispatch)
 
@@ -15,6 +17,7 @@ export default function Result({ poster, title, esrb, year, genre, actors, plot,
 
   const handleClickPoster = async () => {
     console.log('handleClickPoster')
+    setActive((prev) => !prev)
   }
 
   const handleClickTrailer = async () => {
@@ -29,118 +32,84 @@ export default function Result({ poster, title, esrb, year, genre, actors, plot,
       dispatch({ type: 'modalClose' })
     }
   }
-  const getImdbClass = () =>
-    imdbRating <= 2
-      ? 'one' //
-      : imdbRating <= 4
-      ? 'two'
-      : imdbRating <= 6
-      ? 'three'
-      : imdbRating <= 8
-      ? 'four'
-      : 'five'
-
-  const ImdbStars = Stars(imdbRating)
 
   return (
     <div className='result-container'>
       <div className='result'>
-        <Button
-          block
-          type='primary'
-          className='poster'
-          onClick={handleClickPoster}>
-          <img
-            src={poster || NoPosterLogo}
-            alt='movie poster'
-          />
-        </Button>
-        <div className='details'>
-          <div className='panel'>
-            {
-              <div className='deets'>
-                <h3 className='title'>{title || 'Untitled'}</h3>
-                <span className={getImdbClass()}>{ImdbStars}</span>
-                <Button
-                  block
-                  loading={youTubeLoading}
-                  disabled={youTubeError}
-                  className='trailer-btn'
-                  type='primary'
-                  onClick={handleClickTrailer}>
-                  <FontAwesomeIcon icon={faFilm} />
-                </Button>
+        {active ? (
+          <Button
+            className='poster'
+            block
+            type='primary'
+            onClick={handleClickPoster}>
+            {poster ? (
+              <img
+                src={poster}
+                alt='movie poster'
+              />
+            ) : (
+              // <div className='noposter'>No poster</div>
+              <Empty
+                image={NoPosterLogo}
+                description='hello'
+              />
+            )}
+          </Button>
+        ) : (
+          <div className='details'>
+            <Button
+              className='back'
+              block
+              type='dashed'
+              onClick={handleClickPoster}>
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </Button>
+            <h3 className='title'>{title || 'Untitled'}</h3>
+            <Stars stars={imdbRating} />
 
-                {year && <div className='year'>{year}</div>}
+            <Button
+              block
+              loading={youTubeLoading}
+              disabled={youTubeError}
+              className='trailer-btn'
+              type='primary'
+              onClick={handleClickTrailer}>
+              <FontAwesomeIcon icon={faFilm} />
+            </Button>
 
-                {esrb && <div className='esrb'>{esrb}</div>}
+            {year && <div className='year'>{year}</div>}
 
-                {plot && <div className='plot'>{plot}</div>}
+            {esrb && <div className='esrb'>{esrb}</div>}
 
-                {actors && actors.length > 0 && (
-                  <div className='actors'>
-                    {actors.split(', ').map((actor, i) => (
-                      <Tag
-                        key={i}
-                        color='#222'
-                        style={{ color: 'white' }}>
-                        {actor}
-                      </Tag>
-                    ))}
-                  </div>
-                )}
+            {plot && <div className='plot'>{plot}</div>}
 
-                {genre && genre.length > 0 && (
-                  <div className='genre'>
-                    {genre.map((g, i) => (
-                      <Tag
-                        color='black'
-                        key={i}>
-                        {g}
-                      </Tag>
-                    ))}
-                  </div>
-                )}
+            {actors && actors.length > 0 && (
+              <div className='actors'>
+                {actors.split(', ').map((actor, i) => (
+                  <Tag
+                    key={i}
+                    color='#222'
+                    style={{ color: 'white' }}>
+                    {actor}
+                  </Tag>
+                ))}
               </div>
-            }
+            )}
+
+            {genre && genre.length > 0 && (
+              <div className='genre'>
+                {genre.map((g, i) => (
+                  <Tag
+                    color='black'
+                    key={i}>
+                    {g}
+                  </Tag>
+                ))}
+              </div>
+            )}
           </div>
-        </div>
+        )}
       </div>
     </div>
-  )
-}
-
-function Stars(stars) {
-  const Star = <FontAwesomeIcon icon={faStar} />
-  return !stars ? (
-    '' //
-  ) : stars <= 2 ? (
-    <Star />
-  ) : stars <= 4 ? (
-    <>
-      <Star />
-      <Star />
-    </>
-  ) : stars <= 6 ? (
-    <>
-      <Star />
-      <Star />
-      <Star />
-    </>
-  ) : stars <= 8 ? (
-    <>
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-    </>
-  ) : (
-    <>
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-    </>
   )
 }
