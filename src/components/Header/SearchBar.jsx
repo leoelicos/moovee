67e2042cedd1ec00f2379db1c4edb5abb9e7c7d0
Leo Wong <leoelicos@gmail.com>
2023-05-24 23:34:51 +1000

    Add dispatch functions to handle state

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,8 +1,30 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Search from 'antd/es/input/Search'
+import cleanQuery from '../../utils/cleanQuery'
+import { useNavigate } from 'react-router-dom'
+import { useContext, useState } from 'react'
+import { MovieContext, MovieDispatchContext } from '../../context'
 
-export default function SearchBar({ handleSubmit, searchText, setSearchText }) {
+export default function SearchBar() {
+  const navigate = useNavigate()
+  const dispatch = useContext(MovieDispatchContext)
+
+  const { searchText } = useContext(MovieContext)
+
+  function handleSubmit(query) {
+    const text = cleanQuery(query)
+    const encoded = encodeURIComponent(text)
+    const nextPage = `/results?q=${encoded}`
+    if (encoded.length > 0) navigate(nextPage)
+    dispatch({ type: 'setSearchText', action: { text } })
+  }
+
+  function handleChange(event) {
+    const text = event.target.value
+    dispatch({ type: 'setSearchText', action: { text } })
+  }
+
   return (
     <Search
       value={searchText}
@@ -12,7 +34,7 @@ export default function SearchBar({ handleSubmit, searchText, setSearchText }) {
       placeholder={'Search for a movie'}
       enterButton={<FontAwesomeIcon icon={faSearch} />}
       onSearch={handleSubmit}
-      onChange={(e) => setSearchText(e.target.value)}
+      onChange={handleChange}
     />
   )
 }
