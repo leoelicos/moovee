afb7a178f02980f97bbdac96ec448db0eb61a886
Leo Wong <leoelicos@gmail.com>
2023-05-24 23:34:58 +1000

    Add dispatch functions to handle state

--- src/components/Header/SearchHistory.jsx
+++ src/components/Header/SearchHistory.jsx
@@ -1,10 +1,22 @@
 import { faCaretSquareDown } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Empty, Select } from 'antd'
-import { useMemo } from 'react'
+import { useContext, useMemo } from 'react'
+import cleanQuery from '../../utils/cleanQuery'
+import { useNavigate } from 'react-router-dom'
+import { MovieDispatchContext } from '../../context'
 
-export default function SearchHistory({ handleSubmit }) {
-  /* mock */
+export default function SearchHistory() {
+  const navigate = useNavigate()
+  const dispatch = useContext(MovieDispatchContext)
+
+  function handleSubmit(query) {
+    const text = cleanQuery(query)
+    const encoded = encodeURIComponent(text)
+    const nextPage = `/results?q=${encoded}`
+    if (encoded.length > 0) navigate(nextPage)
+    dispatch({ type: 'setSearchText', action: { text } })
+  }
 
   const historyOptions = useMemo(
     () =>
