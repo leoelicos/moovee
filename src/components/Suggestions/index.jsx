53da3b9fca3e73b2f472cf25f839cfac3d70f24f
Leo Wong <leoelicos@gmail.com>
2023-05-24 23:35:47 +1000

    Add dispatch functions to handle state

--- src/components/Suggestions/index.jsx
+++ src/components/Suggestions/index.jsx
@@ -1,16 +1,20 @@
-import { useEffect } from 'react'
+import { useContext, useEffect } from 'react'
 import useTMDB from '../../hooks/useTMDB.js'
 
 import { useNavigate } from 'react-router-dom'
 import cleanQuery from '../../utils/cleanQuery.js'
 import PopularMovie from './PopularMovie.jsx'
 import SuggestionsHeading from './SuggestionsHeading.jsx'
+
+import { MovieDispatchContext } from '../../context/index.jsx'
+
 export default function Suggestions() {
   const { loading, movieTitles, search } = useTMDB()
   const colors = ['magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple']
 
+  const dispatch = useContext(MovieDispatchContext)
+
   useEffect(() => {
-    console.log('useEffect Suggestions')
     search()
   }, [search])
 
@@ -18,8 +22,10 @@ export default function Suggestions() {
 
   const handleClick = (text) => {
     const cleanedQuery = cleanQuery(text)
-    const nextPage = `/results?q=${cleanedQuery}`
+    const encoded = encodeURIComponent(cleanedQuery)
+    const nextPage = `/results?q=${encoded}`
     navigate(nextPage)
+    dispatch({ type: 'setSearchText', action: { text: cleanedQuery } })
   }
 
   if (loading) return 'loading'
