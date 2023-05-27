9a69087af6ad1522abac8e9809143eff7cec50f5
Leo Wong <leoelicos@gmail.com>
2023-05-28 02:32:08 +1000

    Replace button  and add loading and error states

--- src/components/Results/Result/Details/index.jsx
+++ src/components/Results/Result/Details/index.jsx
@@ -1,38 +1,27 @@
 import BackButton from './BackButton/index.jsx'
 import TrailerButton from './TrailerButton/index.jsx'
 import './style/details.css'
-
-import useOMDBById from '../../../../hooks/useOMDBById.js'
-import GetDetailsButton from './GetDetailsButton/index.jsx'
 import MovieData from './MovieData/index.jsx'
 
 export default function Details({
   togglePoster,
   title,
   year,
-  imdbID //
+  loading,
+  error,
+  data //
 }) {
-  const { loading, error, data, search } = useOMDBById()
-
-  const handleClickGetDetailsButton = () => {
-    console.log('handleClickGetDetailsButton')
-    search(imdbID)
-  }
-
   return (
     <div className='details'>
       <TrailerButton
         title={title}
         year={year}
       />
-
-      <GetDetailsButton
-        onClick={handleClickGetDetailsButton}
+      <MovieData
+        data={data}
         loading={loading}
         error={error}
       />
-      <MovieData data={data} />
-
       <BackButton onClick={togglePoster} />
     </div>
   )
