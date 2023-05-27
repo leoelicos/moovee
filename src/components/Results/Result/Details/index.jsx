43f12ff1cb45995264874d198db04125664820f9
Leo Wong <leoelicos@gmail.com>
2023-05-27 23:34:28 +1000

    Abstract components into smaller files

--- src/components/Results/Result/Details/index.jsx
+++ src/components/Results/Result/Details/index.jsx
@@ -1,75 +1,39 @@
-import { Tag, Button } from 'antd'
+import BackButton from './BackButton/index.jsx'
+import TrailerButton from './TrailerButton/index.jsx'
+import './style/details.css'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faFilm } from '@fortawesome/free-solid-svg-icons'
-
-import Stars from '../Stars.jsx'
+import useOMDBById from '../../../../hooks/useOMDBById.js'
+import GetDetailsButton from './GetDetailsButton/index.jsx'
+import MovieData from './MovieData/index.jsx'
 
 export default function Details({
   togglePoster,
   title,
-  imdbRating,
-  youTubeLoading,
-  youTubeError,
-  handleClickTrailer,
   year,
-  esrb,
-  plot,
-  actors,
-  genre //
+  imdbID //
 }) {
+  const { loading, error, data, search } = useOMDBById()
+
+  const handleClickGetDetailsButton = () => {
+    console.log('handleClickGetDetailsButton')
+    search(imdbID)
+  }
+
   return (
     <div className='details'>
-      <Button
-        className='back'
-        block
-        type='dashed'
-        onClick={togglePoster}>
-        <FontAwesomeIcon icon={faArrowLeft} />
-      </Button>
-      <h3 className='title'>{title || 'Untitled'}</h3>
-      <Stars stars={imdbRating} />
+      <TrailerButton
+        title={title}
+        year={year}
+      />
 
-      <Button
-        block
-        loading={youTubeLoading}
-        disabled={youTubeError}
-        className='trailer-btn'
-        type='primary'
-        onClick={handleClickTrailer}>
-        <FontAwesomeIcon icon={faFilm} />
-      </Button>
+      <GetDetailsButton
+        onClick={handleClickGetDetailsButton}
+        loading={loading}
+        error={error}
+      />
+      <MovieData data={data} />
 
-      {year && <div className='year'>{year}</div>}
-
-      {esrb && <div className='esrb'>{esrb}</div>}
-
-      {plot && <div className='plot'>{plot}</div>}
-
-      {actors && actors.length > 0 && (
-        <div className='actors'>
-          {actors.split(', ').map((actor, i) => (
-            <Tag
-              key={i}
-              color='#222'
-              style={{ color: 'white' }}>
-              {actor}
-            </Tag>
-          ))}
-        </div>
-      )}
-
-      {genre && genre.length > 0 && (
-        <div className='genre'>
-          {genre.map((g, i) => (
-            <Tag
-              color='black'
-              key={i}>
-              {g}
-            </Tag>
-          ))}
-        </div>
-      )}
+      <BackButton onClick={togglePoster} />
     </div>
   )
 }
