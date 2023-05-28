dad329d5a75a3e4b85f7a1394d6a14317f2af99e
Leo Wong <leoelicos@gmail.com>
2023-05-29 08:32:08 +1000

    Use actors if available

--- src/components/Results/Result/Details/TrailerButton/index.jsx
+++ src/components/Results/Result/Details/TrailerButton/index.jsx
@@ -8,17 +8,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilm } from '@fortawesome/free-solid-svg-icons'
 
 import './style/trailer-button.css'
+import { querySerialize } from '../../../../../utils/querySerialize'
 
-const getTrailerString = (title, year) => `"${title || ''}"+${year || ''}+trailer`
+const getTrailerString = ({ title, year, data }) => {
+  const { actors } = data
+  let d = ['trailer']
+  if (title) d.push(title)
+  if (year) d.push(year)
+  if (actors) d.push(actors.split(', ')[0])
+  const joined = d.join('+')
+  const trailerString = querySerialize(joined)
+  console.log({ trailerString })
+  return trailerString
+}
 
-export default function TrailerButton({ title, year }) {
+export default function TrailerButton({ title, year, data }) {
   const dispatch = useContext(MovieDispatchContext)
 
-  const { loading, error, data, searchYouTube } = useYouTube()
+  const { loading, error, searchYouTube } = useYouTube()
 
   const handleClickTrailer = async () => {
     try {
-      const query = getTrailerString(title, year)
+      const query = getTrailerString({ title, year, data })
       const tempData = await searchYouTube(query)
       // console.log('new data', { tempData })
       dispatch({ type: 'gapiData', action: { data: tempData } })
