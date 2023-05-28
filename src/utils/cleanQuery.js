b00ad86525aa74cc8720a75d38afc9e3e5dbb07a
Leo Wong <leoelicos@gmail.com>
2023-05-29 06:06:24 +1000

    Add to allowed characters

--- src/utils/cleanQuery.js
+++ src/utils/cleanQuery.js
@@ -1,6 +1,6 @@
 export default function cleanQuery(query) {
   return query //
     .trim()
-    .replace(/([^A-Za-z0-9 ])/gm, '') // invalids with blank
+    .replace(/([^A-Za-z0-9 ':;,./!@#$%^&*()`~])/gm, '') // invalids with blank
     .replace(/(\s{2,})/gm, ' ') // space+ with space
 }
